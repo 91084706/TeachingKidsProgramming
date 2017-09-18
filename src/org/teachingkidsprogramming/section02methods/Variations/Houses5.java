09ac3ad9ff402d8859bd325404dcecb55824810c
91084706 <31664280+91084706@users.noreply.github.com>
2017-09-18 10:57:07 -0700

    started variation 5 of Houses.java

---
+++ src/org/teachingkidsprogramming/section02methods/Variations/Houses5.java
@@ -0,0 +1,75 @@
+package org.teachingkidsprogramming.section02methods.Variations;
+
+import org.teachingextensions.logo.Tortoise;
+import org.teachingextensions.logo.utils.ColorUtils.PenColors;
+import org.teachingextensions.virtualproctor.VirtualProctor;
+
+public class Houses5
+{
+  public static void main(String[] args)
+  {
+    VirtualProctor.setClassName("Palm Digital");
+    VirtualProctor.setName("Fiona");
+    Tortoise.show();
+    Tortoise.setSpeed(10);
+    Tortoise.setX(200);
+    int height = 40;
+    drawHouse(height);
+    height = 120;
+    drawHouse(height);
+    height = 90;
+    drawHouse(height);
+    height = 20;
+    drawHouse(height);
+  }
+  private static void drawHouse(int height)
+  {
+    Tortoise.setPenColor(PenColors.Reds.Red);
+    Tortoise.move(height);
+    drawDomeRoof();
+    Tortoise.move(height);
+    Tortoise.turn(-90);
+    Tortoise.move(20);
+    Tortoise.turn(-90);
+  }
+  private static void drawDomeRoof()
+  {
+    for (int i = 0; i < 360; i++)
+    {
+      Tortoise.move(0.5);
+      Tortoise.turn(2);
+    }
+  }
+  private static void drawTrapezoidRoof()
+  {
+    Tortoise.turn(25);
+    Tortoise.move(20);
+    Tortoise.turn(65);
+    Tortoise.move(20);
+    Tortoise.turn(70);
+    Tortoise.move(20);
+    Tortoise.turn(20);
+  }
+  private static void drawSlantedRoof()
+  {
+    Tortoise.move(30);
+    Tortoise.turn(120);
+    Tortoise.move(40);
+    Tortoise.turn(60);
+    Tortoise.move(10);
+  }
+  private static void drawPointyRoof()
+  {
+    Tortoise.turn(45);
+    Tortoise.move(15);
+    Tortoise.turn(90);
+    Tortoise.move(15);
+    Tortoise.turn(45);
+  }
+  private static void drawFlatRoof()
+  {
+    Tortoise.turn(90);
+    Tortoise.move(30);
+    Tortoise.turn(90);
+  }
+}
