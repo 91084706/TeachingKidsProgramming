5f602c7dc11f06c64edb7a15ac7c666bfcf2650c
91084706 <31664280+91084706@users.noreply.github.com>
2017-12-06 08:25:53 -0800

    started FizzBuzz

--- src/org/teachingkidsprogramming/section09final/FizzBuzz.java
+++ src/org/teachingkidsprogramming/section09final/FizzBuzz.java
@@ -3,9 +3,29 @@ package org.teachingkidsprogramming.section09final;
 public class FizzBuzz
 {
   //   For the whole numbers from 1 to 100, print either that number, or, 
-  //    If that number is divisible by 3, then print the word 'Fizz', 
-  //    If that number is divisible by 5, then print the word 'Buzz', 
-  //    If that number is divisible by 3 and 5, then print the word 'FizzBuzz'
+  int num = 0;
+  //for or if
+  {
+    //    If that number is divisible by 3, then print the word 'Fizz', 
+    if (num % 3 == 0)
+    {
+      System.out.println("Fizz");
+    }
+    //    If that number is divisible by 5, then print the word 'Buzz', 
+    else if (num % 5 == 0)
+    {
+      System.out.println("Buzz");
+    }
+    //    If that number is divisible by 3 and 5, then print the word 'FizzBuzz'
+    else if (num % 15 == 0)
+    {
+      System.out.println("FizzBuzz");
+    }
+    else
+    {
+      System.out.println(num);
+    }
+  }
   //
   //    NOTE: this is a kata (higher level instructions)
   //    part of the exercise is to translate into line-by-line English, THEN Java
